04ed63b2e9f72e414ddc955d1294b4907fa81151
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-12-10 01:45:00 +0530

    added toaster

--- frontend/src/components/chats/groupChats/WakuChat.jsx
+++ frontend/src/components/chats/groupChats/WakuChat.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useWaku, useLightPush, useFilterMessages } from "@waku/react";
 import { createEncoder, createDecoder } from "@waku/sdk";
 import protobuf from 'protobufjs';
+import toast, { Toaster } from 'react-hot-toast';
 
 const WakuChat = () => {
     const [inputMessage, setInputMessage] = useState("");
@@ -16,7 +17,7 @@ const WakuChat = () => {
     const { node, error, isLoading } = useWaku();
 
     // Create a message encoder and decoder
-    const contentTopic = "/waku-react-guide/1/chat/proto";
+    const contentTopic = "/waku-react-guide/2/chat/proto";
     const encoder = createEncoder({ contentTopic });
     const decoder = createDecoder(contentTopic);
 
@@ -31,25 +32,27 @@ const WakuChat = () => {
 
 
     const sendMessage = async () => {
-        if (!push || inputMessage.length === 0) return;
+        try {
+            if (!push || inputMessage.length === 0) return;
 
-        // Create a new message object
-        const timestamp = Date.now();
-        const protoMessage = ChatMessage.create({
-            timestamp: timestamp,
-            message: inputMessage
-        });
+            // Create a new message object
+            const timestamp = Date.now();
+            const protoMessage = ChatMessage.create({
+                timestamp: timestamp,
+                message: inputMessage
+            });
 
-        // Serialise the message and push to the network
-        const payload = ChatMessage.encode(protoMessage).finish();
-        const { recipients, errors } = await push({ payload, timestamp });
-
-        // Check for errors
-        if (errors.length === 0) {
-            setInputMessage("");
-            console.log("MESSAGE PUSHED");
-        } else {
-            console.log(errors);
+            // Serialise the message and push to the network
+            const payload = ChatMessage.encode(protoMessage).finish();
+            const { recipients, errors } = await push({ payload, timestamp });
+            if (errors.length === 0) {
+                setInputMessage("");
+                toast.success("Message sent!");
+            } else {
+                toast.error(errors[0]);
+            }
+        } catch (error) {
+            toast.error(error.message);
         }
     };
 
@@ -66,13 +69,13 @@ const WakuChat = () => {
 
     return (
         <>
+            <Toaster />
             <div className="chat-interface">
-                <h1 className='text-white'>Community</h1>
-                <h1 className='text-white'>Community</h1>
+                <h1 className='text-white font-jost text-4xl font-bold p-4'>Community</h1>
                 <div className="chat-body">
                     {messages.map((message, index) => (
                         <div key={index} className="chat-message">
-                            <span>{new Date(message.timestamp).toUTCString()}</span>
+                            <span className='text-white'>{new Date(message.timestamp).toUTCString()}</span>
                             <div className="message-text">{message.message}</div>
                         </div>
                     ))}
@@ -85,7 +88,7 @@ const WakuChat = () => {
                         onChange={handleInputChange}
                         placeholder="Type your message..."
                     />
-                    <button className="send-button" onClick={sendMessage}>Send</button>
+                    <button className="btn" onClick={sendMessage}>Send</button>
                 </div>
             </div>
         </>
--- frontend/src/pages/chat.js
+++ frontend/src/pages/chat.js
@@ -94,6 +94,22 @@ const Chat = () => {
                     />
                 </ChatUIProvider> */}
             </div>
+            <div className="chat chat-start">
+                <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                        <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                </div>
+                <div className="chat-bubble">You were the Chosen One!</div>
+            </div>
+            <div className="chat chat-end">
+                <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                        <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                </div>
+                <div className="chat-bubble">I hate you!</div>
+            </div>
         </div>
     );
 };
